6959154f0fa030f26b9d87a174b14c2a32b3cee4
mariarafiq <mrafiq@scripps.edu>
2025-04-01 17:53:50 -0700

    Initial commit of bootcamp utilities.

---
+++ jb_bootcamp/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for utilities for bootcamp."""
+
+from .na_utils import *
+from .bioinfo_dicts import *
+
+__author__ = 'maria rafiq'
+__email__ = 'mrafiq@scripps.edu'
+__version__ = '0.0.1'
